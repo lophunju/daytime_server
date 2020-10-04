df4bccb97c7bbe84bbc915be7acbe696816869cb
root <root@ip-172-31-42-44.ap-northeast-2.compute.internal>
2020-10-04 01:38:59 +0000

    Client로의 response 테스트 완료 및 2
    - response to client test ok
    - error handling 작성
    - wrapper function 작성

--- main.c
+++ main.c
@@ -12,6 +12,150 @@
 #define MAXLINE 1024
 #define LISTENQ 5
 
+/* headers for error handling */
+#include <stdarg.h>	// varying arguments (va_~~~), vsnprintf()
+#include <errno.h>	// errno
+#include <stdlib.h> // exit()
+//#include <stdio.h> // vsnprintf(), snprintf(), fflush() (already included)
+#include <string.h>	// strlen(), strerror(), strcat()
+#include <syslog.h>	// syslog(), LOG_ERR
+
+#define MAXLINE_E 100
+
+
+/**
+ * for error handling 
+ */
+int daemon_proc;	// set nonzero by daemon_init(), only works when the process is daemonized
+
+static void err_doit(int, int, const char*, va_list);
+
+/* Fatal error related to system call
+ * Print message, dump core, and terminate */
+void err_sys(const char *fmt, ...){
+	
+	va_list ap;	// pointer indicating the first of variable arguments
+	
+	va_start(ap, fmt);	// make ap indicate after the last fixed argument
+	// could simply get variable arguments like 'var = va_arg(ap, type)'
+	err_doit(1, LOG_ERR, fmt, ap);	// 1/0: print errno or not, LOG_ERR: level-error conditions
+
+	va_end(ap);	// the end of using variable arguments
+	exit(1);	// abnormal termination
+}
+
+/* Fatal error unrelated to system call
+ * Print message and terminate */
+void err_quit(const char *fmt, ...){
+	// similar to err_sys
+
+	va_list ap;
+
+	va_start(ap, fmt);
+	// could simply get variable arguments like 'var = va_arg(ap, type)'
+	err_doit(0, LOG_ERR, fmt, ap);
+
+	va_end(ap);
+	exit(1);
+}
+
+/* Print message and return to caller
+ * Caller specifies "errnoflag" and "level" */
+static void err_doit(int errnoflag, int level, const char* fmt, va_list ap){
+
+	int errno_save, n;
+	char buf[MAXLINE_E + 1];
+
+	// errno: integer variable, set by system calls and some library functions on error
+	// to use, errno should be saved to prevent from being changed by other calls
+	errno_save = errno;
+
+	vsnprintf(buf, MAXLINE_E, fmt, ap);	// vsnprintf() invokes va_arg macro
+	// could use vsprintf when vsnprintf doesn't work, but not safe
+
+	n = strlen(buf);
+	if (errnoflag)
+		snprintf(buf+n, MAXLINE_E-n, ": %s", strerror(errno_save));	// strerror() returns the string describes the errno_save
+	strcat(buf, "\n");
+
+	if (daemon_proc){	// only works when the process is daemonized
+		syslog(level, "%s", buf);	// format string should be comes to 2nd argument
+	} else {
+		fflush(stdout);		// in case stdout and stderr are the same
+		fputs(buf, stderr);
+		fflush(stderr);
+	}
+	
+	return;
+}
+
+
+
+
+/**
+ * wrapper functions
+ * Socket, Bind, Listen, Accept, Write, Close
+ */
+int Socket(int domain, int type, int protocol){
+	
+	int n;
+	if ( (n = socket(domain, type, protocol)) < 0 )
+		err_sys("socket error");
+
+	return n;	// sockfd integer number on success
+}
+
+int Bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen){
+	
+	int n;
+	if ( (n = bind(sockfd, addr, addrlen)) < 0 ) {
+		err_sys("bind error");
+	}
+
+	return n;	// 0 on success
+}
+
+int Listen(int sockfd, int backlog){
+
+	int n;
+	if ( (n = listen(sockfd, backlog)) < 0 ) {
+		err_sys("listen error");
+	}
+
+	return n;	// 0 on success
+}
+
+int Accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen){
+
+	int n;
+	if ( (n = accept(sockfd, addr, addrlen)) < 0 )
+		err_sys("accept error");
+
+	return n;	// sockfd integer number on success
+}
+
+ssize_t Write(int fd, const void* buf, size_t count){
+
+	int n;
+	if ( (n = write(fd, buf, count)) < 0 )
+		err_sys("write error");
+
+	return n;	// the number of bytes written on success
+}
+
+int Close(int fd){
+
+	int n;
+	if ( (n = close(fd)) < 0 ) {
+		err_sys("close error");
+	}
+
+	return n;	// 0 on success
+}
+
+
+
+
 int main(int argc, char **argv){
 
 	int listenfd, connfd;
